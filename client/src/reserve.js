fe9d683a94c935c60effda85b3db4a5a75d1afc9
Shayon Maramkhah <maramkhah002@wcsu.edu>
2021-02-12 17:55:29 -0500

    flow

--- client/src/reserve.js
+++ client/src/reserve.js
@@ -1,15 +1,28 @@
 import './App.css';
-import React from "react";
+import React, {useState,useEffect}from "react";
 
 export default function ComponentTwo(){
+const[Name,setName]=useState("");
+const[Phone,setPhone]=useState("");
 
-
+useEffect(()=>{
+  var pg = document.getElementById("i");
+  pg.innerHTML=Name+" "+Phone;
+})
 
   return (
    
     <div className="App">
       <header className="App-header">
-       HELLO
+       <form>
+         <input onChange={(e)=>{
+           setName(e.target.value)
+         }} type="text" placeholder="John Doe"></input>
+         <input onChange={(e)=>{
+           setPhone(e.target.value)
+         }}type="text" placeholder="Cell phone"></input>
+       </form>
+       <p id="i">chg me</p>
       </header>
     </div>
   );
