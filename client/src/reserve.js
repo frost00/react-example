766328f8fb0acfbff4aa18a53d91fee4e6bde7c2
Shayon Maramkhah <maramkhah002@wcsu.edu>
2021-02-13 08:12:06 -0500

    maps

--- client/src/reserve.js
+++ client/src/reserve.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, {useState,useEffect}from "react";
 import {Loader} from "@googlemaps/js-api-loader"
+import axios from "axios"
 
 export default function ComponentTwo(){
 const[Name,setName]=useState("");
@@ -10,6 +11,28 @@ useEffect(()=>{
   var pg = document.getElementById("i");
   pg.innerHTML=Name+" "+Phone;
 })
+
+
+const options = {
+  method: 'GET',
+  url: 'https://google-maps-geocoding.p.rapidapi.com/geocode/json',
+  params: {address: '164 Townsend St., San Francisco, CA', language: 'en'},
+  headers: {
+    'x-rapidapi-key': '91f52bc93cmsh54e965aa41a23d9p16e486jsn1418eef8f96f',
+    'x-rapidapi-host': 'google-maps-geocoding.p.rapidapi.com'
+  }
+};
+
+
+
+const btnsubmit =() =>{
+  axios.request(options).then(function (response) {
+    const obj = response.data;
+    console.log(obj.results[0].formatted_address);
+  }).catch(function (error) {
+    console.error(error);
+  });
+}
 
 const google = window.google = window.google? window.google:{};
 //let map;
@@ -21,10 +44,10 @@ const loader = new Loader({
 });
 const mapOptions ={
   center:{
-   lat:0,
-  lng:0
+   lat:41.19374829854264,
+  lng:-73.21673112434215
   },
-  zoom: 4
+  zoom: 11
 };
 
 loader.load().then(()=>{
@@ -39,14 +62,15 @@ loader.load().then(()=>{
     <div className="App">
       <header className="App-header">
       <p id="i">chg me</p>
-      <div id="map">SOMETHING</div>
+      <div id="map"></div>
        <form id="mapForm">
          <input onChange={(e)=>{
            setName(e.target.value)
-         }} type="text" placeholder="John Doe"></input>
+         }} type="text" placeholder="Street"></input>
          <input onChange={(e)=>{
            setPhone(e.target.value)
          }}type="text" placeholder="Cell phone"></input>
+        <button id="btn" onClick={btnsubmit} type="button">Submit</button>
        </form>
       </header>
     </div>
